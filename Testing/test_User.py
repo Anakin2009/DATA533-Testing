2a4e9f1a0b1c6a6c01c77f9a95c79cb8d2f436a6
Anakin2009 <kcjzj007@gmail.com>
2022-12-13 17:37:23 -0800

    add files

--- Testing/test_User.py
+++ Testing/test_User.py
@@ -1,3 +1,19 @@
 import unittest # Jupyter notebook
 import User
-class TestCalculator(unittest.TestCase): # test class
+class TestUser(unittest.TestCase): # test class
+
+    def setUp(self):
+        print('Set up')
+    def tearDown(self):
+        print('Tear Down')
+    def test_genAccNum(self): # test case
+        self.assertEqual(User.genAccNum(), 10000)
+        self.assertEqual(User.genAccNum(), 33333)
+        self.assertEqual(User.genAccNum(), 22)
+        self.assertEqual(User.genAccNum(), 999999)
+
+unittest.main(argv=[''], verbosity=2, exit=False)
+
+
+
+
