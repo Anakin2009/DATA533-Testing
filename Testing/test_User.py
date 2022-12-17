631eebc97ea2a9796c17e7ff2c58cf1a0b44f9b1
Anakin2009 <kcjzj007@gmail.com>
2022-12-17 13:58:44 -0800

    edited

--- Testing/test_User.py
+++ Testing/test_User.py
@@ -1,48 +1,69 @@
 
 
 import unittest # Jupyter notebook
-from User import *
-from Calculation import *
+from BankAccountSystem.Structure import *
+
+class User():
+    def __init__(self, name, acc_num):
+        self.name = name
+        self.acc_num = acc_num
+
+class newUser(User):
+    def __init__(self, name, acc_num, init_dps):
+        super().__init__(name, acc_num)
+        self.balance = init_dps
+        self.init_dps = init_dps
+
+    def information(self, date):
+        print("Here is your account information")
+        print(f'Name: {self.name}')
+        print(f'Account Number: {self.acc_num}')
+        print(f'Open Date: {date}')
+        print(f'Account Balance: {self.balance}')
+
+    def store(self):
+        ds.addAccount(self.name, self.acc_num, self.init_dps, self.balance)
+
 
 
 class TestUser(unittest.TestCase): # test class
-    def __int__(self):
-        return super().__int__()
 
     def setUp(self):
-        self.newuser1 = newUser('John', 10001, 10)
-        self.newuser2 = newUser('Mary', 10002, 20)
-        self.newuser3 = newUser('Tom', 10003, 30)
-        self.newuser4 = newUser('Jack', 10004, 40)
+        self.user1 = User('John', 10001)
+        self.user2 = User('Mary', 10002)
+        self.user3 = User('Tom', 10003)
+        self.user4 = User('Jack', 10004)
+        self.user1 = newUser('John', 10001, 10)
+        self.user2 = newUser('Mary', 10002, 20)
+        self.user3 = newUser('Tom', 10003, 30)
+        self.user4 = newUser('Jack', 10004, 50)
+
 
     def tearDown(self):
         print('Tear Down')
 
-    def setUpClass(newUser):
+    @classmethod
+    def setUpClass(User):
         print('SetupClass')
 
-    def tearDownClass(newUser):
+    @classmethod
+    def tearDownClass(cls):
         print('TearDownClass')
 
-    def test_deposit(self):
-        self.newuser1.deposit('2020-01-01')
-        self.assertEqual(self.newuser1.balance, 10)
-        self.newuser2.deposit('2020-01-02')
-        self.assertEqual(self.newuser2.balance, 20)
-        self.newuser3.deposit('2020-01-03')
-        self.assertEqual(self.newuser3.balance, 30)
-        self.newuser4.deposit('2020-01-04')
-        self.assertEqual(self.newuser4.balance, 50)
+    def test_user(self):
+        self.assertEqual(self.user1.name, 'John')
+        self.assertEqual(self.user2.name, 'Mary')
+        self.assertEqual(self.user3.name, 'Tom')
+        self.assertEqual(self.user4.name, 'Jacck')
 
-    def test_information(self):
-        self.newuser1.information('2020-01-01')
-        self.newuser2.information('2020-01-02')
-        self.newuser3.information('2020-01-03')
-        self.newuser4.information('2020-01-04')
-        self.assertEqual(self.newuser1.acc_num, 10001)
-        self.assertEqual(self.newuser2.acc_num, 10002)
-        self.assertEqual(self.newuser3.acc_num, 10003)
-        self.assertEqual(self.newuser4.acc_num, 10005)
+    def test_newUser(self):
+        self.assertEqual(self.user1.balance, 10)
+        self.assertEqual(self.user2.balance, 20)
+        self.assertEqual(self.user3.balance, 30)
+        self.assertEqual(self.user4.balance, 980)
+
+    if __name__ == '__main__':
+        unittest.main(argv=[''], verbosity=2, exit=False)
 
 
-unittest.main(argv=[''], verbosity=2, exit=False)
+#%%
